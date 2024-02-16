0c7f500bf1d4dcb3343f235799e770b523cc7209
hotsauceuniverse <jangsy6976@naver.com>
2024-02-16 19:05:25 +0900

    commit

--- app/src/main/java/com/example/googleplaystoreapi/HomeActivity.java
+++ app/src/main/java/com/example/googleplaystoreapi/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.googleplaystoreapi;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.services.androidpublisher.AndroidPublisher;
 import com.google.api.services.androidpublisher.AndroidPublisherScopes;
+import com.google.api.services.androidpublisher.model.SubscriptionPurchase;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,6 +22,7 @@ public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "PlayStoreApi";
     TextView user_name, user_email;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +51,11 @@ public class HomeActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids) {
             try {
                 InputStream credentialStream = HomeActivity.this.getAssets().open("rosy-slate-414404-9c5971dc79a0");
+                Log.d("asdss1", "asdss" + credentialStream);
+
                 GoogleCredential credential = GoogleCredential.fromStream(credentialStream)
                         .createScoped(Collections.singleton(AndroidPublisherScopes.ANDROIDPUBLISHER));
+                Log.d("asdss2", "asdss" + credential);
 
                 AndroidPublisher androidPublisher = new AndroidPublisher.Builder(
                         credential.getTransport(),
@@ -57,10 +63,12 @@ public class HomeActivity extends AppCompatActivity {
                         credential)
                         .setApplicationName("GooglePlayStoreAPI")
                         .build();
+                Log.d("asdss3", "asdss" + androidPublisher);
 
                 String packageName = "카트라이더 러쉬플러스";
 //                AndroidPublisher.Purchases.Subscriptions.List request = androidPublisher.purchases().subscriptions().list(packageName);
 //                AndroidPublisher.Purchases.Subscriptions.ListResponse response = request.execute();
+//                SubscriptionPurchase purchase = androidPublisher.purchases().subscriptions().get(packageName, "jangse8931@gmail.com", ).execute();
 
             } catch (IOException e) {
                 Log.e(TAG, "Error : " + e.getMessage());
